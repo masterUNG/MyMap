2da5b1afa57ac4e75f9a16ff2282db7757460a2a
masterUNG <phrombutr@gmail.com>
2016-01-07 18:00:45 +0700

    Finish 7Jan16

--- app/src/main/java/appewtc/masterung/mymap/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mymap/MainActivity.java
@@ -1,17 +1,79 @@
 package appewtc.masterung.mymap;
 
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView showLatTextView, showLngTextView;
+    private LocationManager objLocationManager;
+    private Criteria objCriteria;
+    private boolean GPSABoolean, networkABoolean;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Open Service Get Location
+        openServiceGetLocation();
+
     }   // Main Method
+
+    private void bindWidget() {
+        showLatTextView = (TextView) findViewById(R.id.txtShowLat);
+        showLngTextView = (TextView) findViewById(R.id.txtShowLng);
+    }
+
+
+    //Create Class for Find Location
+    public final LocationListener objLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            showLatTextView.setText(String.format("%.7f", location.getLatitude()));
+            showLngTextView.setText(String.format("%.7f", location.getLongitude()));
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };  // End of Class
+
+
+    private void openServiceGetLocation() {
+
+        objLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        objCriteria = new Criteria();
+        objCriteria.setAccuracy(Criteria.ACCURACY_FINE);
+        objCriteria.setAltitudeRequired(false);
+        objCriteria.setBearingRequired(false);
+
+    }   // openServiceGetLocation
 
     public void clickMyMap(View view) {
 
