89c8f0f3f9e37a6236141e79a0dfbe9a1e9512dc
masterUNG <phrombutr@gmail.com>
2016-01-07 17:21:17 +0700

    Create Map API2 Finish

--- app/src/main/java/appewtc/masterung/mymap/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mymap/MainActivity.java
@@ -1,7 +1,9 @@
 package appewtc.masterung.mymap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   // Main Method
+
+    public void clickMyMap(View view) {
+
+        Intent objIntent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(objIntent);
+
     }
-}
+
+}   // Main Class
