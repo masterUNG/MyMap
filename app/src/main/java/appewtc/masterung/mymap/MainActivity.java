ecbaeb1d947a5aa720592c6ccbd18cd6edc7ca6b
masterUNG <phrombutr@gmail.com>
2016-01-11 17:19:07 +0700

    onStart near finish

--- app/src/main/java/appewtc/masterung/mymap/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mymap/MainActivity.java
@@ -1,12 +1,16 @@
 package appewtc.masterung.mymap;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,19 +37,68 @@ public class MainActivity extends AppCompatActivity {
 
     }   // Main Method
 
-    public Location requestUpdateFromProvider(String strProvider, String strError) {
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        GPSABoolean = objLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+
+
+    }   // onStart
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setupForRestart();
+    }
+
+    private void setupForRestart() {
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        objLocationManager.removeUpdates(objLocationListener);
+
+    }
+
+    public Location requestLocation(String strProvider, String strError) {
 
         Location objLocation = null;
 
         if (objLocationManager.isProviderEnabled(strProvider)) {
 
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
             objLocationManager.requestLocationUpdates(strProvider, 1000, 10, objLocationListener);
+            objLocation = objLocationManager.getLastKnownLocation(strProvider);
 
         } else {
             Toast.makeText(MainActivity.this, strError, Toast.LENGTH_SHORT).show();
         }
 
-        return null;
+        return objLocation;
     }
 
     private void bindWidget() {
@@ -83,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void openServiceGetLocation() {
 
-        objLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        objLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         objCriteria = new Criteria();
         objCriteria.setAccuracy(Criteria.ACCURACY_FINE);
         objCriteria.setAltitudeRequired(false);
