9b03493a1fdd3daf71a3ed4391231649f8b5d960
masterUNG <phrombutr@gmail.com>
2016-01-15 17:57:19 +0700

    Change Type Map

--- app/src/main/java/appewtc/masterung/mymap/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/mymap/MapsActivity.java
@@ -2,6 +2,8 @@ package appewtc.masterung.mymap;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,7 +13,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
     private String centerLatString, centerLngString;
@@ -19,11 +21,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Double[] myLatDoubles = {13.66938156, 13.66531579, 13.66431498, 13.67071595};
     private Double[] myLngDoubles = {100.62350035, 100.6226635, 100.61676264, 100.61671972};
     private LatLng[] myLatLngs;
+    private Button normalButton, satelliteButton, terrainButton, hybridButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_maps);
+
+        //Bind Widget
+        bindWidget();
 
         //Create LatLng
         createLatLng();
@@ -34,6 +40,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.fragment);
         mapFragment.getMapAsync(this);
     }   // Main Method
+
+    private void bindWidget() {
+        normalButton = (Button) findViewById(R.id.button2);
+        satelliteButton = (Button) findViewById(R.id.button3);
+        terrainButton = (Button) findViewById(R.id.button4);
+        hybridButton = (Button) findViewById(R.id.button5);
+    }
 
     private void createLatLng() {
 
@@ -90,7 +103,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .icon(BitmapDescriptorFactory
                         .defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
 
+        changeTypeMap();
+
 
     }   // onMapReady
 
+    private void changeTypeMap() {
+
+        normalButton.setOnClickListener(this);
+        satelliteButton.setOnClickListener(this);
+        terrainButton.setOnClickListener(this);
+        hybridButton.setOnClickListener(this);
+
+    }   // changeTypeMap
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.button2:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.button3:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.button4:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.button5:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+        }   // switch
+
+    }   // onClick
 }   // Main Class
