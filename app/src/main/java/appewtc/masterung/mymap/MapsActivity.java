852125941e67ea014e7bee425d435df6790bf980
masterUNG <phrombutr@gmail.com>
2016-01-15 16:45:08 +0700

    Learn Find Center Map

--- app/src/main/java/appewtc/masterung/mymap/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/mymap/MapsActivity.java
@@ -1,25 +1,27 @@
 package appewtc.masterung.mymap;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private String centerLatString, centerLngString;
+    private LatLng centerLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        //Create LatLng
+        createLatLng();
 
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -28,15 +30,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }   // Main Method
 
+    private void createLatLng() {
+
+        //For Center Map
+        centerLatString = getIntent().getStringExtra("Lat");
+        centerLngString = getIntent().getStringExtra("Lng");
+
+        //Change Data Type
+        Double latDouble = Double.parseDouble(centerLatString);
+        Double lngDouble = Double.parseDouble(centerLngString);
+
+        centerLatLng = new LatLng(latDouble, lngDouble);
+
+    }   // createLatLng
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Setup Center Map
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centerLatLng, 16));
+
     }   // onMapReady
 
 }   // Main Class
